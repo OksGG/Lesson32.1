b22c7b57f3d45f323b25a19d2ade6f23ae4fc697
OGG <oks2.6@yandex.ru>
2021-04-07 01:18:40 +0300

    Initial commit

---
+++ src/test/java/PageObjectTest.java
@@ -0,0 +1,44 @@
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import page.BasePage;
+import page.LoginPage;
+import page.ProductsPage;
+
+
+public class PageObjectTest extends BasePage {
+    LoginPage loginPage;
+    ProductsPage productsPage;
+
+
+    public PageObjectTest() {
+        super();
+    }
+
+    @Before
+    public void init() {
+        initialization();
+        loginPage = new LoginPage();
+        productsPage = new ProductsPage();
+
+    }
+
+    @Test
+    public void pageTest() {
+        LoginPage.login();
+        ProductsPage.shopping();
+    }
+
+
+    @Test
+    public void pageTestInfo() {
+        LoginPage.login();
+        ProductsPage.checkoutInfo();
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
